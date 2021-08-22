c4a202d6e07d96b7dcb7725ab7992ea5b8c8c6b6
Zovitch <zovitch@gmail.com>
2021-08-22 19:15:06 +0800

    user model, validation, webtoken, jwt,

--- config/db.js
+++ config/db.js
@@ -4,17 +4,19 @@ const db = config.get('mongoURI');
 
 // we use a try as to prevent crash if connection errors to the database
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        });
-        console.log('MongoDB connected...');
-    } catch(err) {
-        console.log('error to catch');
-        console.log(err.message);
-        // Exit process with failure
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      userCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.log('error to catch');
+    console.log(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API running'));
 
@@ -19,4 +19,4 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , () => console.log('Server started on port ${PORT}'));
+app.listen(PORT, () => console.log('Server started on port ${PORT}'));
