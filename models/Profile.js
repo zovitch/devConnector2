c021c57a80cd4634b56901b5879a533c7f0aab0d
zovitch <zovitch@gmail.com>
2021-08-25 11:28:26 +0800

    Add & delte Profile Education

--- models/Profile.js
+++ models/Profile.js
@@ -68,7 +68,7 @@ const ProfileSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
-      fieldofstrudy: {
+      fieldofstudy: {
         type: String,
         required: true,
       },
