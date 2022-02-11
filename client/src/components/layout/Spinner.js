4fe64147d83e5a94e6c4d526fc850677053eb753
zovitch <zovitch@gmail.com>
2022-02-11 14:15:21 +0800

    dashboard start

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className='py-3 text-center'>
+      <i className='fas fa-spinner fa-spin fa-2x' />
+    </div>
+  );
+};
+export default Spinner;
