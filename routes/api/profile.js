404b164b09507c0013e161d061cfc2d124cf6bab
zovitch <zovitch@gmail.com>
2022-02-15 15:50:08 +0800

    FIX skills profile split

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -73,7 +73,9 @@ router.post(
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
     if (skills) {
-      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+      profileFields.skills = skills
+        .split(',')
+        .map((skill) => ' ' + skill.trim());
     }
 
     // Build social object
