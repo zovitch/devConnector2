a215afb481c3190923b30bc276fd54eb7dad484e
zovitch <zovitch@gmail.com>
2022-02-15 16:18:01 +0800

    populate profile get route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,15 +13,15 @@ const User = require('../../models/User');
 // @access  Private
 router.get('/me', auth, async (req, res) => {
   try {
-    const profile = await Profile.findOne({ user: req.user.id }).populate(
-      'user',
-      ['name', 'avatar']
-    );
+    const profile = await Profile.findOne({ user: req.user.id });
 
     if (!profile) {
       return res.status(400).json({ msg: 'There is no profile for this user' });
     }
-    res.json(profile);
+    // @update
+    // only populate from user document if profile exists
+
+    res.json(profile.populate('user', ['name', 'avatar']));
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
