1ca24ce77cd7c605586b1a5c8dd2335e53093636
zovitch <zovitch@gmail.com>
2022-02-17 17:41:56 +0800

    fix state issue #60

--- client/src/reducers/profiles.js
+++ client/src/reducers/profiles.js
@@ -36,6 +36,7 @@ export default function (state = initialState, action) {
       return {
         ...state,
         error: payload,
+        profile: null,
         loading: false,
       };
     case GET_REPOS:
