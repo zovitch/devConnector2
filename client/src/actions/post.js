6efa8748c36346d0215cb6c8f9737c1d549f75a1
zovitch <zovitch@gmail.com>
2022-02-24 11:40:34 +0800

    Single Post Display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,6 +4,7 @@ import {
   DELETE_POST,
   ADD_POST,
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
 } from './types';
@@ -88,6 +89,22 @@ export const addPost = (formData) => async (dispatch) => {
       payload: res.data,
     });
     dispatch(setAlert('Post Added', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get post
+export const getPost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
